d3f08613aac4ba3b76bfa665298c31f8a1bf5ef3
Jean <acalto2@gmail.com>
2023-05-06 22:28:03 +0200

    intro,setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:riverpod_rivaan/features/screens/login_screen.dart';
 import 'package:riverpod_rivaan/theme/pallete.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Reddit Tutorial',
       theme: Pallete.darkModeAppTheme,
-      home: SizedBox(),
+      home: LoginScreen(),
     );
   }
 }
