ab01f86c2bb2b9e38b72b46d29e8bd8b7e69f6dd
Jean <acalto2@gmail.com>
2023-05-09 19:15:15 +0200

    creating communities and storing to firebase

---
+++ lib/features/community/repository/community_repository.dart
@@ -0,0 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:riverpod_rivaan/core/constants/firebase_constants.dart';
+import 'package:riverpod_rivaan/core/failure.dart';
+import 'package:riverpod_rivaan/core/providers/firebase_providers.dart';
+import 'package:riverpod_rivaan/core/type_defs.dart';
+import 'package:riverpod_rivaan/models/community_model.dart';
+
+final communityRepositoryProvider = Provider((ref) {
+  return CommunityRepository(
+    firestore: ref.watch(firestoreProvider),
+  );
+});
+
+class CommunityRepository {
+  final FirebaseFirestore _firestore;
+  CommunityRepository({required FirebaseFirestore firestore})
+      : _firestore = firestore;
+
+  FutureVoid createCommunity(Community community) async {
+    try {
+      var communityDoc = await _communities.doc(community.name).get();
+      if (communityDoc.exists) {
+        throw 'Community with the sane name exists!';
+      }
+      return right(_communities.doc(community.name).set(community.toMap()));
+    } on FirebaseException catch (e) {
+      return left(Failure(e.message!)); // equal to "throw e.message!;"
+    } catch (e) {
+      return left(Failure(e.toString()));
+    }
+  }
+
+  CollectionReference get _communities =>
+      _firestore.collection(FirebaseConstants.communitiesCollection);
+}
