0c3947366dbe78a5a867dc6a13dff67b79de72f6
Jean <acalto2@gmail.com>
2023-05-15 00:57:52 +0200

    fetching feed posts

--- lib/core/common/post_card.dart
+++ lib/core/common/post_card.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_rivaan/core/constants/constants.dart';
 import 'package:riverpod_rivaan/features/auth/controller/auth_controller.dart';
 import 'package:riverpod_rivaan/models/post_model.dart';
 import 'package:riverpod_rivaan/theme/pallete.dart';
+import 'package:any_link_preview/any_link_preview.dart';
 
 class PostCard extends ConsumerWidget {
   final Post post;
@@ -10,9 +12,9 @@ class PostCard extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // final isTypeImage = post.type == 'image';
-    // final isTypeText = post.type == 'text';
-    // final isTypeLink = post.type == 'link';
+    final isTypeImage = post.type == 'image';
+    final isTypeText = post.type == 'text';
+    final isTypeLink = post.type == 'link';
     final user = ref.watch(userProvider)!;
 
     final currenTheme = ref.watch(themeNotifierProvider);
@@ -34,6 +36,7 @@ class PostCard extends ConsumerWidget {
                         horizontal: 16,
                       ).copyWith(right: 0),
                       child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -79,6 +82,93 @@ class PostCard extends ConsumerWidget {
                                 ),
                             ],
                           ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10.0),
+                            child: Text(
+                              post.title,
+                              style: const TextStyle(
+                                  fontSize: 19, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          if (isTypeImage)
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.35,
+                              width: double.infinity,
+                              child: Image.network(
+                                post.link!,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          if (isTypeLink)
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.35,
+                              width: double.infinity,
+                              child: AnyLinkPreview(
+                                displayDirection:
+                                    UIDirection.uiDirectionHorizontal,
+                                link: post.link!,
+                              ),
+                            ),
+                          if (isTypeText)
+                            Container(
+                              alignment: Alignment.bottomLeft,
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 15.0),
+                              child: Text(
+                                post.description!,
+                                style: const TextStyle(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ),
+                          Row(
+                            children: [
+                              Row(
+                                children: [
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(
+                                      Constants.up,
+                                      size: 30,
+                                      color: post.upvotes.contains(user.uid)
+                                          ? Pallete.redColor
+                                          : null,
+                                    ),
+                                  ),
+                                  Text(
+                                    '${post.upvotes.length - post.downvotes.length == 0 ? "Vote" : post.upvotes.length - post.downvotes.length}',
+                                    style: const TextStyle(fontSize: 17),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(
+                                      Constants.down,
+                                      size: 30,
+                                      color: post.downvotes.contains(user.uid)
+                                          ? Pallete.blueColor
+                                          : null,
+                                    ),
+                                  ),
+
+
+                                ],
+                              ),
+                               Row(
+                                  children: [
+                                    IconButton(
+                                      onPressed: (){},// => navigateToComments(context),
+                                      icon: const Icon(
+                                        Icons.comment,
+                                      ),
+                                    ),
+                                    Text(
+                                      '${post.commentCount == 0 ? 'Comment' : post.commentCount}',
+                                      style: const TextStyle(fontSize: 17),
+                                    ),
+                                  ],
+                                ),
+                            ],
+                          )
                         ],
                       ),
                     ),
